c3185719d6d32267f5838b4c24b2f0afca1d6937
islamov.t <islamov.t@freshforex.com>
2024-11-28 10:38:28 +0500

    fix route

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\MathController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/math/fibonachi', [MathController::class, 'fibonacci']);
+Route::get('/math/fibonacci', [MathController::class, 'fibonacci']);
 Route::get('/math/is-prime', [MathController::class, 'isPrime']);
